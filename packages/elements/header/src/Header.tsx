fab68abb367dc99aebfd4bd884d915d0e1acdedf
Viktor Shyrokov <viktor.shyrokov@toptal.com>
2022-08-22 10:16:16 +0200

    changed color of the button

--- packages/elements/header/src/Header.tsx
+++ packages/elements/header/src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Header() {
     return (
         <header style={{
-            color: 'blue'
+            color: 'green'
         }}>
             Lerna is the original monorepo tool!11111
         </header>
