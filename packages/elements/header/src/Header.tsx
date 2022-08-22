959a5c34595194a9effdcb9b308ecf7b88d8a3a4
Viktor Shyrokov <viktor.shyrokov@toptal.com>
2022-08-22 10:23:25 +0200

    changed color of the footer

--- packages/elements/header/src/Header.tsx
+++ packages/elements/header/src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Header() {
     return (
         <header style={{
-            color: 'green'
+            color: 'yellow'
         }}>
             Lerna is the original monorepo tool!11111
         </header>
